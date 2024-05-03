955d0e20b8747dcc7a13b0602e08678272dabbc3
Wei-Cheng Pan <legnaleurc@gmail.com>
2024-05-03 16:11:14 +0900

    chore!: migrate to node.js

--- dfd/view.py
+++
@@ -1,7 +0,0 @@
-from aiohttp.web import View
-import aiohttp_jinja2 as aj
-
-
-class IndexHandler(View):
-    async def get(self):
-        return aj.render_template("index.html", self.request, None)
