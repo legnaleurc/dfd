308ff8fb8633312416f561984ef77859b392e578
Wei-Cheng Pan <legnaleurc@gmail.com>
2025-05-02 15:30:41 +0900

    chore: migrate drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
+export default defineConfig({
   schema: "./src/lib/server/schema.ts",
   out: "./drizzle/migrations",
-  driver: "better-sqlite",
+  dialect: "sqlite",
   dbCredentials: {
-    url: process.env.SQLITE_FILE,
+    url: process.env.SQLITE_FILE ?? "",
   },
-} satisfies Config;
+});
