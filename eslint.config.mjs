220208c277aaf1ebd86445b25c987a0320bf5426
Wei-Cheng Pan <legnaleurc@gmail.com>
2025-05-02 14:49:44 +0900

    chore: migrate eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import svelteParser from "svelte-eslint-parser";
+import sveltePlugin from "eslint-plugin-svelte";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...sveltePlugin.configs.recommended,
+  prettier,
+  {
+    files: ["**/*.{js,ts,svelte}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: 2020,
+        extraFileExtensions: [".svelte"],
+      },
+      globals: {
+        browser: true,
+        es2017: true,
+        node: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      svelte: sveltePlugin,
+    },
+  },
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  {
+    ignores: [
+      ".DS_Store",
+      "node_modules/**",
+      "build/**",
+      ".svelte-kit/**",
+      "package/**",
+      ".env",
+      ".env.*",
+      "!.env.example",
+      "pnpm-lock.yaml",
+      "package-lock.json",
+      "yarn.lock",
+    ],
+  },
+);
